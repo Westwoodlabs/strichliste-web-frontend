57225e11ff2890b3a2f88cc9b5d64108ee5cfc99
pmenke <info@pmenke.de>
2024-08-13 22:11:14 +0200

    WIP improve on foorschtbars commit

--- src/components/common/idle-timer.tsx
+++ src/components/common/idle-timer.tsx
@@ -8,22 +8,23 @@ let timerId: any = 0;
 export function useIdleTimer(onTimeOut: () => void) {
   const settings = useSettings();
 
-  const resetTimer = (...args) => {
-    let defaultTimer = settings.common.idleTimeout;
-    if (args.length > 2) {
-      defaultTimer = args[2];
-    }
-
+  const resetTimer = (timeout?: number, ...args: any[]) => {
+    let defaultTimer = timeout ?? settings.common.idleTimeout;
     clearTimeout(timerId);
-    timerId = setTimeout(onTimeOutCheck, defaultTimer);
+    timerId = setTimeout(onTimeOutCheck, defaultTimer, ...args);
   };
 
   // Prevent onTimeOut if we are on a input field
-  const onTimeOutCheck = () => {
-
-    if (document.activeElement && document.activeElement.tagName === 'INPUT' && document.activeElement.id !== "scanner") {
+  const onTimeOutCheck = (...args: any[]) => {
+    // on first timout check, we check if we are on an input field,
+    // if we are, we reset the timer to the remaining time of the idleTimeoutOnInput.
+    // if that time passes as well, we will call onTimeOut.
+    // we also call onTimeOut immediately if we are not on an input field.
+    // the 'scanner' element doesn't count as an input field.
+    let isIdleOnInput = args.length > 0 && args[0] === 'input-idle-check';
+    if (!isIdleOnInput && document.activeElement && document.activeElement.tagName === 'INPUT' && document.activeElement.id !== "scanner") {
       console.log('Idle timeout prevented. Current active element:', document.activeElement);
-      resetTimer(settings.common.idleTimeoutOnInput - settings.common.idleTimeout);
+      resetTimer(settings.common.idleTimeoutOnInput - settings.common.idleTimeout, 'input-idle-check');
     } else {
       console.log('Idle timeout passed. Current active element:', document.activeElement);
       onTimeOut();
@@ -32,15 +33,16 @@ export function useIdleTimer(onTimeOut: () => void) {
 
   React.useEffect(() => {
     resetTimer();
-    document.addEventListener('scroll', resetTimer);
-    document.addEventListener('click', resetTimer);
-    document.addEventListener('touch', resetTimer);
-    document.addEventListener('keyup', resetTimer);
+    let doReset = () => resetTimer(); //use default timeout
+    document.addEventListener('scroll', doReset);
+    document.addEventListener('click', doReset);
+    document.addEventListener('touch', doReset);
+    document.addEventListener('keyup', doReset);
     return () => {
-      document.removeEventListener('scroll', resetTimer);
-      document.removeEventListener('click', resetTimer);
-      document.removeEventListener('touch', resetTimer);
-      document.removeEventListener('keyup', resetTimer);
+      document.removeEventListener('scroll', doReset);
+      document.removeEventListener('click', doReset);
+      document.removeEventListener('touch', doReset);
+      document.removeEventListener('keyup', doReset);
       clearTimeout(timerId);
     };
   }, []);
